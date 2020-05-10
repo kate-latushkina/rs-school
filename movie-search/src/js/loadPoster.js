785155694b19a135d67d92ccebe1de7d6a61a27c
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-05-10 21:13:11 +0300

    fix: some bugs

--- movie-search/src/js/loadPoster.js
+++ movie-search/src/js/loadPoster.js
@@ -2,13 +2,13 @@ async function loadPoster(cardPoster, link) {
   const image = cardPoster
   try {
     if (link === 'N/A') {
-      image.setAttribute('src', '../img/no-poster.jpg')
+      image.setAttribute('src', './img/no-poster.jpg')
     } else {
       await fetch(link)
       image.setAttribute('src', `${link}`)
     }
   } catch (error) {
-    image.setAttribute('src', '../img/24451278.jpg')
+    image.setAttribute('src', './img/24451278.jpg')
   }
 }
 export default loadPoster
