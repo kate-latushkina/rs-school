8550fcdbe5efb5cb51f6e713589a155f9d181d4c
kate-latushkina <59828320+kate-latushkina@users.noreply.github.com>
2020-05-22 22:04:47 +0300

    feat: add eslint and normalize

--- fancy-weather/.eslintrc.js
+++ fancy-weather/.eslintrc.js
@@ -16,5 +16,18 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "linebreak-style": ["error", "windows"],
+        quotes: ["error", "single", { allowTemplateLiterals: true }],
+        semi: ["error", "never"],
+        "no-use-before-define": ["error", { functions: false, variables: false }],
+        "operator-linebreak": [2, "before"],
+        "no-restricted-syntax": [
+            "error",
+            "WithStatement",
+            "BinaryExpression[operator='in']"
+        ],
+        "no-unused-expressions": [2, { allowTernary: true }],
+        "import/extensions": [2, "ignorePackages", { js: "always" }],
+        "object-curly-newline": ["error", { multiline: true }],
     }
 };
